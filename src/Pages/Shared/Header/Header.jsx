4f876101b21cfd88d031d0e9d568ce1c3eb819ef
Shihan Chowdhury <hackedgdown@gmail.com>
2023-04-29 00:03:37 +0100

    ML-10-Module-62-4 done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Header.css';
+import logo from '/public/img/logo.png';
+import moment from 'moment/moment';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
+        <div className="container">
+            <div className="header-top text-center">
+                <div className="header-logo">
+                    <img src={logo} alt="Logo" />  
+                </div>
+                <h1 className="header-title">Journalism Without Fear or Favour</h1>
+                <h3 className="header-time">{moment().format('dddd, MMMM D, YYYY')}</h3>
+            </div>
         </div>
     );
 };
