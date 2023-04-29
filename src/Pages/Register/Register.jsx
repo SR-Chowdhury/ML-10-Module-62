193d56b06f57b2f4cb28f0b6c23afae124e25069
Shihan Chowdhury <hackedgdown@gmail.com>
2023-04-29 23:35:26 +0100

    64-1 Recap Route and get registration data done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,33 +1,65 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Register = () => {
+
+    const { user, createUser } = useContext(AuthContext);
+
+    const [success, setSuccess] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+
+        setError('');
+        setSuccess('');
+
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirmPassword = form.confirmPassword.value;
+
+        if (password !== confirmPassword) setError('Password Did not matched!');
+        else if (password.length < 6) setError('Password length should be six characters!')
+
+        createUser(email, password)
+            .then(reult => {
+                const loggedInUser = reult.user;
+                setError('');
+                setSuccess('Successfully Created User!');
+                form.reset();
+            })
+            .catch(err => setError(err.message))
+    }
+
     return (
         <Container>
 
-            <Form className='formContainer'>
+            <Form className='formContainer' onSubmit={handleSubmit}>
                 <h1 className='titleForm'>Register Account</h1>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Your Name</Form.Label>
-                    <Form.Control type="text" name="name" placeholder="Enter Your Name" />
+                    <Form.Control type="text" name="name" required placeholder="Enter Your Name" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Control type="email" name="email" required placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="*******" />
+                    <Form.Control type="password" name="password" required placeholder="*******" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" name="confirmPassword" placeholder="*******" />
+                    <Form.Control type="password" name="confirmPassword" required placeholder="*******" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Accept Terms & Conditions" />
@@ -36,6 +68,14 @@ const Register = () => {
                     <button className='formBtn' type="submit">Register</button>
                 </div>
                 <p className='text-muted text-center mt-3'>Already Have An Account ? <Link to="/login">Login</Link></p>
+                {
+                    error && <p className="text-danger text-center">{error}</p>
+                }
+                {
+                    success && <p className="text-success text-center">{success}</p>
+                }
+
+
             </Form>
         </Container>
     );
